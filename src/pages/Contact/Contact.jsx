646eb9dd96bd2c8e7407a59ffaa0c2fc3a8c1c70
Natalia Panova <khlipochenko@gmail.com>
2025-02-22 17:52:27 +0100

    in del

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -51,12 +51,12 @@ export const Contact = () => {
                   <FaGithub />
                 </a>
               </li>
-              <li>
+              {/* <li>
                 {" "}
                 <a className="contact-page__profile-item" href="#">
                   <FaLinkedin />
                 </a>
-              </li>
+              </li> */}
               <li>
                 {" "}
                 <a className="contact-page__profile-item" href="https://wa.me/+4915126702692">
